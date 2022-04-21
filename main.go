fef5d8bb05e9657355005c888b0f5335c5d34c71
GisangLee <masterkorea01@naver.com>
2022-04-21 11:36:24 +0900

    [ in progress ] Marshal Text

--- main.go
+++ main.go
@@ -13,8 +13,15 @@ import (
 
 const PORT string = ":4000"
 
+type URL string
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", PORT, u)
+	return []byte(url), nil
+}
+
 type URLDescription struct {
-	URL         string `json:"url"`
+	URL         URL    `json:"url"`
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload,omitempty"`
@@ -23,12 +30,12 @@ type URLDescription struct {
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL:         "/",
+			URL:         URL("/"),
 			Method:      "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL:         "/block",
+			URL:         URL("/block"),
 			Method:      "POST",
 			Description: "Add a Block",
 			Payload:     "data:string",
