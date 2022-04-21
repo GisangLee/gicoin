e8d24802be1cac5998f230949f0e9fc4a1022b2c
GisangLee <masterkorea01@naver.com>
2022-04-21 10:20:33 +0900

    [ fix ] rendering block

--- main.go
+++ main.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/gisanglee/gicoin/blockchain"
 )
 
 const PORT string = ":4000"
+const TEMPLATE_DIR string = "templates/"
+
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "hello from home")
+
+	data := homeData{PageTitle: "Home", Blocks: blockchain.AllBlocks()}
+
+	templates.ExecuteTemplate(rw, "home", data)
+
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(TEMPLATE_DIR + "pages/*.html"))
+	templates = template.Must(templates.ParseGlob(TEMPLATE_DIR + "partials/*.html"))
+
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", PORT)
 	log.Fatal(http.ListenAndServe(PORT, nil))
