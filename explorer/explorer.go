3c49754b6809a31641cdf7113641bce04fa4d546
GisangLee <masterkorea01@naver.com>
2022-04-21 10:44:58 +0900

    [ refactor and finish ] rendering block

---
+++ explorer/explorer.go
@@ -0,0 +1,54 @@
+package explorer
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gisanglee/gicoin/blockchain"
+)
+
+const PORT string = ":4000"
+const TEMPLATE_DIR string = "explorer/templates/"
+
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+
+	data := homeData{PageTitle: "Home", Blocks: blockchain.AllBlocks()}
+
+	templates.ExecuteTemplate(rw, "home", data)
+
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func Start() {
+
+	templates = template.Must(template.ParseGlob(TEMPLATE_DIR + "pages/*.html"))
+	templates = template.Must(templates.ParseGlob(TEMPLATE_DIR + "partials/*.html"))
+
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+
+	fmt.Printf("Listening on http://localhost%s\n", PORT)
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
--- main.go
+++ main.go
@@ -1,53 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	"github.com/gisanglee/gicoin/blockchain"
-)
-
-const PORT string = ":4000"
-const TEMPLATE_DIR string = "templates/"
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-
-	data := homeData{PageTitle: "Home", Blocks: blockchain.AllBlocks()}
-
-	templates.ExecuteTemplate(rw, "home", data)
-
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
+import "github.com/gisanglee/gicoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(TEMPLATE_DIR + "pages/*.html"))
-	templates = template.Must(templates.ParseGlob(TEMPLATE_DIR + "partials/*.html"))
-
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-
-	fmt.Printf("Listening on http://localhost%s\n", PORT)
-	log.Fatal(http.ListenAndServe(PORT, nil))
+	explorer.Start()
 }
