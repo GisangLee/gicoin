ca86a64e0372c34028ae72cb88a49e48f40530e9
GisangLee <masterkorea01@naver.com>
2022-04-21 11:09:43 +0900

    [ add ] REST API 1. JSON

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gisanglee/gicoin/utils"
+)
+
 //import "github.com/gisanglee/gicoin/explorer"
+
+const PORT string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleError(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
 	//explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("LIstening on http://localhost%s\n", PORT)
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
