ce01e4a6316d007cb1a71f8e62a26f9d409d9e37
GisangLee <masterkorea01@naver.com>
2022-04-21 11:23:38 +0900

    [ in progress ] REST API 2. Marshal & struct field tag

--- main.go
+++ main.go
@@ -14,9 +14,10 @@ import (
 const PORT string = ":4000"
 
 type URLDescription struct {
-	URL         string
-	Method      string
-	Description string
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
@@ -26,10 +27,17 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Method:      "GET",
 			Description: "See Documentation",
 		},
+		{
+			URL:         "/block",
+			Method:      "POST",
+			Description: "Add a Block",
+			Payload:     "data:string",
+		},
 	}
+	rw.Header().Add("Content-Type", "application/json")
 	b, err := json.Marshal(data)
 	utils.HandleError(err)
-	fmt.Printf("%s", b)
+	fmt.Fprintf(rw, "%s", b)
 }
 
 func main() {
