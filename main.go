88ebb96b130d08806e6f9447ac041957a809df18
GisangLee <masterkorea01@naver.com>
2022-04-22 10:10:05 +0900

    [ start ] CLI

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"github.com/gisanglee/gicoin/explorer"
-	"github.com/gisanglee/gicoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to Gi Coin\n\n")
+	fmt.Printf("Please use the following commands: \n\n")
+	fmt.Printf("explorer:    Start the HTML Explorer\n")
+	fmt.Printf("rest:        Start the REST API (recommended)\n")
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(3000)
-	rest.Start(5000)
+	//go explorer.Start(3000)
+	//rest.Start(5000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }
