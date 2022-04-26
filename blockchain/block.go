c48567ebac69e4f6fd9e87556379dbccd996ba46
GisangLee <masterkorea01@naver.com>
2022-04-26 09:59:19 +0900

    [ Mining ] POW

--- blockchain/block.go
+++ blockchain/block.go
@@ -9,11 +9,15 @@ import (
 	"github.com/gisanglee/gicoin/utils"
 )
 
+const difficulty int = 2
+
 type Block struct {
-	Data     string `json:"data"`
-	Hash     string `json:"hash"`
-	PrevHash string `json:"previous_hash,omitempty"`
-	Height   int    `json:"height"`
+	Data       string `json:"data"`
+	Hash       string `json:"hash"`
+	PrevHash   string `json:"previous_hash,omitempty"`
+	Height     int    `json:"height"`
+	Difficulty int    `json:"difficulty"`
+	Nonce      int    `json:"nonce"`
 }
 
 func (b *Block) persist() {
--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"github.com/gisanglee/gicoin/cli"
-	"github.com/gisanglee/gicoin/db"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	defer db.Close()
+	//defer db.Close()
 	//go explorer.Start(3000)
 	//rest.Start(5000)
 	//blockchain.Blockchain()
-	cli.Start()
+	//cli.Start()
+	difficulty := 4
+	target := strings.Repeat("0", difficulty)
+
+	nonce := 1
+
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
+		fmt.Printf("Hash : %s\nTarget: %s\nNonce: %d\n\n", hash, target, nonce)
+		if strings.HasPrefix(hash, target) {
+			return
+		}
+
+		nonce++
+	}
 }
