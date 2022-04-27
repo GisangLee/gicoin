bc333f324205ba102915b4b381fd9a1e9e97c373
GisangLee <masterkorea01@naver.com>
2022-04-27 11:09:43 +0900

    [ wallets ] Signature

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/gisanglee/gicoin/cli"
-	"github.com/gisanglee/gicoin/db"
+	"github.com/gisanglee/gicoin/wallet"
 )
 
 func main() {
-	defer db.Close()
+	//defer db.Close()
 	//go explorer.Start(3000)
 	//rest.Start(5000)
 	//blockchain.Blockchain()
-	cli.Start()
+	//cli.Start()
+	wallet.Start()
 }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,1 +1,30 @@
 package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/gisanglee/gicoin/utils"
+)
+
+func Start() {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+
+	utils.HandleError(err)
+
+	message := utils.Hash("I Love You")
+
+	byteMessage, err := hex.DecodeString(message)
+
+	utils.HandleError(err)
+
+	r, s, err := ecdsa.Sign(rand.Reader, privateKey, byteMessage)
+
+	utils.HandleError(err)
+
+	fmt.Printf("R:%d\nS:%d\n\n", r, s)
+
+}
